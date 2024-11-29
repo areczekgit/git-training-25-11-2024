09121e2c575c54dd473159b268dd77d71f89c5e2
areczekgit <arkadiuszbak@icloud.com>
2024-11-29 09:53:15 +0100

    Added random number generator

--- src/main/java/eu/chrost/git/Main.java
+++ src/main/java/eu/chrost/git/Main.java
@@ -1,8 +1,13 @@
 package eu.chrost.git;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello Word - Arek");
+
+        Random rand = new Random();
+        int value = rand.nextInt(100);
+        System.out.println(value);
     }
 }
