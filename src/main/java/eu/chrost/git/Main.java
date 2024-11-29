05299b62e542b4dd7c401d64555022bd835ce441
areczekgit <arkadiuszbak@icloud.com>
2024-11-29 09:39:58 +0100

    Added random generator

--- src/main/java/eu/chrost/git/Main.java
+++ src/main/java/eu/chrost/git/Main.java
@@ -1,8 +1,15 @@
 package eu.chrost.git;
 
+imort java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello Word - Arek");
+
+        Random rand = new Random();
+        int value = rand.nextInt(100);
+        System.out.println(value);
     }
 }
+
